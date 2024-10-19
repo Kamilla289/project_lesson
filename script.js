b72dc9e552c3061c534a4c83459aea0ca2a843d6
Kamilla289 <kami.rivay56@gmail.com>
2024-10-19 16:47:47 +0500

    calculation price js

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-let title = 'AVOC'; 
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 221;
+'use strict';
+
+let title = prompt('Как называется ваш проект?')
+console.log(title);
+
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+console.log(screens);
+
+let screenPrice = prompt('Сколько будет стоить данная работа?', '12000');
+console.log(screenPrice);
+
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+console.log(adaptive);
+
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+
+let fullPrice = screenPrice += servicePrice1 += servicePrice2;
+console.log('Сумма:', fullPrice);
+
+
 let rollback = 5;
-let fullPrice = 600000;
-let adaptive = true;
+let servicePercentPrice = fullPrice - fullPrice * rollback / 100;
+console.log(Math.ceil(servicePercentPrice));
+
+
+switch (true) {
+  case fullPrice > 30000:
+          console.log("Даем скидку в 10%");
+      break;
+  case fullPrice > 15000 && fullPrice < 30000:
+          console.log("Даем скидку в 5%");
+      break;
+  case fullPrice < 15000 && fullPrice > 0:
+          console.log("Скидка не предусмотрена");
+      break;
+  case fullPrice <= 0:
+          console.log ("Что-то пошло не так")
+};
+
 
 console.log(typeof title);
 console.log(typeof fullPrice);
